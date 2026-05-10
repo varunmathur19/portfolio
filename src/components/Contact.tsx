dc70919e2d167a2a304f46f59fccec51507ab92a
varunmathur19 <varunmathur817@gmail.com>
2026-05-10 18:13:45 +0530

    first commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,57 +11,52 @@ const Contact = () => {
             <h4>Email</h4>
             <p>
               <a href="mailto:example@mail.com" data-cursor="disable">
-                example@mail.com
+                varunmathur817@gmail.com
               </a>
             </p>
             <h4>Phone</h4>
             <p>
               <a href="tel:+9199999999" data-cursor="disable">
-                +91 99999 99999
+                +91 9990221402
               </a>
             </p>
           </div>
-          <div className="contact-box">
-            <h4>Social</h4>
-            <a
-              href="https://github.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Github <MdArrowOutward />
-            </a>
-            <a
-              href="https://www.linkedin.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Linkedin <MdArrowOutward />
-            </a>
-            <a
-              href="https://x.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Twitter <MdArrowOutward />
-            </a>
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Instagram <MdArrowOutward />
-            </a>
-          </div>
+      <div className="contact-box">
+  <h4>Social</h4>
+
+  <a
+    href="https://github.com/varunmathur19"
+    target="_blank"
+    data-cursor="disable"
+    className="contact-social"
+  >
+    Github <MdArrowOutward />
+  </a>
+
+  <a
+    href="https://www.linkedin.com/in/varun-mathur-2874a8319/"
+    target="_blank"
+    data-cursor="disable"
+    className="contact-social"
+  >
+    Linkedin <MdArrowOutward />
+  </a>
+
+  <a
+    href="https://www.instagram.com/_varunmathur/"
+    target="_blank"
+    data-cursor="disable"
+    className="contact-social"
+  >
+    Instagram <MdArrowOutward />
+  </a>
+</div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Moncy Yohannan</span>
+              Designed and Developed <br /> by <span>Varun Mathur</span>
             </h2>
             <h5>
-              <MdCopyright /> 2024
+              <MdCopyright /> 2026
             </h5>
           </div>
         </div>
