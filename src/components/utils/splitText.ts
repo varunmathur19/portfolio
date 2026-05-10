4e0fba07843c25eefa71e5e780f4be060553b5bc
varunmathur19 <varunmathur817@gmail.com>
2026-05-10 18:30:19 +0530

    add the button

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -5,22 +5,30 @@ import SplitText from "gsap-trial/SplitText";
 
 interface ParaElement extends HTMLElement {
   anim?: gsap.core.Animation;
-  split?: SplitText;
+  split?: any;
 }
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 export default function setSplitText() {
   ScrollTrigger.config({ ignoreMobileResize: true });
-  if (window.innerWidth < 900) return;
-  const paras: NodeListOf<ParaElement> = document.querySelectorAll(".para");
-  const titles: NodeListOf<ParaElement> = document.querySelectorAll(".title");
 
-  const TriggerStart = window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
+  if (window.innerWidth < 900) return;
+
+  const paras: NodeListOf<ParaElement> =
+    document.querySelectorAll(".para");
+
+  const titles: NodeListOf<ParaElement> =
+    document.querySelectorAll(".title");
+
+  const TriggerStart =
+    window.innerWidth <= 1024 ? "top 60%" : "20% 60%";
+
   const ToggleAction = "play pause resume reverse";
 
   paras.forEach((para: ParaElement) => {
     para.classList.add("visible");
+
     if (para.anim) {
       para.anim.progress(1).kill();
       para.split?.revert();
@@ -30,6 +38,8 @@ export default function setSplitText() {
       type: "lines,words",
       linesClass: "split-line",
     });
+
+    if (!para.split?.words) return;
 
     para.anim = gsap.fromTo(
       para.split.words,
@@ -48,15 +58,20 @@ export default function setSplitText() {
       }
     );
   });
+
   titles.forEach((title: ParaElement) => {
     if (title.anim) {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
+
     title.split = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+
+    if (!title.split?.chars) return;
+
     title.anim = gsap.fromTo(
       title.split.chars,
       { autoAlpha: 0, y: 80, rotate: 10 },
